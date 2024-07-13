7ad9804a50e26fd1af42b8e970754384a6159f3a
Subina Dhakal <subiakal1212@gmail.com>
2024-07-13 23:17:33 +0545

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GoogleMock.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
